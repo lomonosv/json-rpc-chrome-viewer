8ff50b2107dedbcb9e6cf7d21851b3d0c92fb311
lomonosv <lomonosv.developer@gmail.com>
2022-06-22 23:39:41 +0300

    HTTP Archive draft context

---
+++ src/logic/HTTPArchive/HttpArchiveContext.tsx
@@ -0,0 +1,33 @@
+import React, { createContext, useContext, useEffect } from 'react';
+
+const useRequest = () => {
+  const handleRequest = (request: chrome.devtools.network.Request) => {
+    console.log(request);
+  };
+
+  useEffect(() => {
+    chrome.devtools.network.onRequestFinished.addListener(handleRequest);
+  }, []);
+
+  return {};
+};
+
+type RequestContextType = ReturnType<typeof useRequest>;
+
+export const RequestContext = createContext<RequestContextType>(null);
+
+export const useRequestContext = (): RequestContextType => (
+  useContext<RequestContextType>(RequestContext)
+);
+
+interface IProps {
+  children: React.ReactElement
+}
+
+const RequestContextProvider: React.FC<IProps> = ({ children }) => (
+  <RequestContext.Provider value={ useRequest() }>
+    { children }
+  </RequestContext.Provider>
+);
+
+export default RequestContextProvider;
