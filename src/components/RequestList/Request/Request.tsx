254f219269c44c0ea92da689674a9777bac9cb71
lomonosv <lomonosv.developer@gmail.com>
2022-06-25 11:44:32 +0300

    Request url

--- src/components/RequestList/Request/Request.tsx
+++ src/components/RequestList/Request/Request.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { IRequest } from '../../../logic/HTTPArchive/IRequest';
+import styles from './request.scss';
 
 const Request = ({ data }: { data: IRequest }) => (
-  <div>
-    { data.requestJSON.method }
+  <div className={ styles.request }>
+    <div className={ styles.method }>
+      { data.requestJSON.method }
+    </div>
+    <div className={ styles.url }>
+      { data.request.url }
+    </div>
   </div>
 );
 
