6ade777369693703072f49a99bec337d09e63b72
lomonosv <lomonosv.developer@gmail.com>
2022-06-25 11:55:40 +0300

    Resizable

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Resizable } from 're-resizable';
 import styles from './layout.scss';
 import RequestList from '../RequestList';
 
 const Layout = () => (
   <div className={ styles.layoutContainer }>
-    <div className={ styles.leftSideContainer }>
+    <Resizable
+      className={ styles.leftSideContainer }
+      defaultSize={ {
+        width: 200,
+        height: '100%'
+      } }
+    >
       <RequestList />
-    </div>
+    </Resizable>
     <div className={ styles.rightSideContainer }>
       <div className={ styles.requestContainer }>
         Request
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import Layout from './components/Layout';
 import SettingsContext from './logic/SettingsContext';
 import HttpArchiveContext from './logic/HTTPArchive/HttpArchiveContext';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('application')).render(
   <SettingsContext>
