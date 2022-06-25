a7af39ee2f0c6f56da3d81d638a489f558a1d359
lomonosv <lomonosv.developer@gmail.com>
2022-06-25 17:05:37 +0300

    Layout

---
+++ src/components/ResponseInfo/ResponseInfo.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styles from './responseInfo.scss';
+import Header from '../common/Header';
+
+const ResponseInfo = () => (
+  <div className={ styles.responseInfoWrapper }>
+    <Header>
+      <>Response</>
+    </Header>
+    <div className={ styles.responseInfoContainer }>
+      <div>TreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTreeTree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+      <div>Tree</div>
+    </div>
+  </div>
+);
+
+export default ResponseInfo;
