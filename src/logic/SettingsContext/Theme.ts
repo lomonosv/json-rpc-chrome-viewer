a4aee3b7e399cba23bb0896277282daeba037d71
lomonosv <lomonosv.developer@gmail.com>
2022-07-16 22:28:29 +0300

    Tree View Theme Selector added

--- src/logic/SettingsContext/Theme.ts
+++ src/logic/SettingsContext/Theme.ts
@@ -1,9 +1,45 @@
-export enum Theme {
+export enum DevToolsTheme {
   Light = 'light',
   Dark = 'dark'
 }
 
 export enum JsonViewerTheme {
-  Light = 'summerfruit:inverted',
-  Dark = 'summerfruit'
+  System = 'System',
+  Apathy = 'Apathy',
+  ApathyInverted = 'Apathy:inverted',
+  Ashes = 'Ashes',
+  Bespin = 'Bespin',
+  Brewer = 'Brewer',
+  BrightInverted = 'Bright:inverted',
+  Bright = 'Bright',
+  Chalk = 'Chalk',
+  Codeschool = 'Codeschool',
+  Colors = 'Colors',
+  Eighties = 'Eighties',
+  Embers = 'Embers',
+  Flat = 'Flat',
+  Google = 'Google',
+  Grayscale = 'Grayscale',
+  GrayscaleInverted = 'Grayscale:inverted',
+  GreenScreen = 'Greenscreen',
+  Harmonic = 'Harmonic',
+  Hopscotch = 'Hopscotch',
+  Isotope = 'Isotope',
+  Marrakesh = 'Marrakesh',
+  Mocha = 'Mocha',
+  Monokai = 'Monokai',
+  Ocean = 'Ocean',
+  Paraiso = 'Paraiso',
+  Pop = 'Pop',
+  Railscasts = 'Railscasts',
+  RjvDefault = 'Rjv-default',
+  Shapeshifter = 'Shapeshifter',
+  ShapeshifterInverted = 'Shapeshifter:inverted',
+  Solarized = 'Solarized',
+  SummerFruit = 'Summerfruit',
+  SummerFruitInverted = 'Summerfruit:inverted',
+  ThreeZeroTwoFour = 'Threezerotwofour',
+  Tomorrow = 'Tomorrow',
+  Tube = 'Tube',
+  Twilight = 'Twilight'
 }
