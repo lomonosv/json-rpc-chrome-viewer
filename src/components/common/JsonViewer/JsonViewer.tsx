b5696f8f076f1a1cd6106a0bc6e018efd591f0a5
lomonosv <lomonosv.developer@gmail.com>
2022-08-08 23:11:29 +0300

    Fixed bug with no reseted tree state

--- src/components/common/JsonViewer/JsonViewer.tsx
+++ src/components/common/JsonViewer/JsonViewer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactJsonView, { ThemeKeys } from 'react-json-view';
 import { JSONValue } from '~/logic/HTTPArchive/IRequest';
 import { useSettingsContext } from '~/logic/SettingsContext/SettingsContext';
@@ -12,26 +12,37 @@ interface IComponentProps {
 }
 
 const JsonViewer = ({ src, defaultOpenNodesDepth = 1, expandTreeState }: IComponentProps) => {
+  const [isInitialized, setIsInitialized] = useState(false);
   const { systemJsonViewerTheme } = useSettingsContext();
 
   const collapsed = expandTreeState === ExpandTreeState.Default
     ? defaultOpenNodesDepth
     : expandTreeState === ExpandTreeState.Collapsed;
 
+  useEffect(() => {
+    setIsInitialized(false);
+
+    setTimeout(() => {
+      setIsInitialized(true);
+    }, 0);
+  }, [src]);
+
   return (
     <div className={ styles.jsonViewer }>
-      <ReactJsonView
-        name={ false }
-        src={ src as object }
-        theme={ systemJsonViewerTheme.toLowerCase() as ThemeKeys }
-        collapsed={ collapsed }
-        enableClipboard={ false }
-        indentWidth={ 2 }
-        displayDataTypes={ false }
-        iconStyle="square"
-        quotesOnKeys={ false }
-        displayObjectSize={ false }
-      />
+      { isInitialized && (
+        <ReactJsonView
+          name={ false }
+          src={ src as object }
+          theme={ systemJsonViewerTheme.toLowerCase() as ThemeKeys }
+          collapsed={ collapsed }
+          enableClipboard={ false }
+          indentWidth={ 2 }
+          displayDataTypes={ false }
+          iconStyle="square"
+          quotesOnKeys={ false }
+          displayObjectSize={ false }
+        />
+      ) }
     </div>
   );
 };
