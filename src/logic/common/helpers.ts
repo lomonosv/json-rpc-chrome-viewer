b0b5addf14ad65545babed4e6b821e24e3f4c0c1
lomonosv <lomonosv.developer@gmail.com>
2023-05-24 14:27:33 +0300

    Fixed convertation response to TS issue

--- src/logic/common/helpers.ts
+++ src/logic/common/helpers.ts
@@ -1,3 +1,6 @@
+import { Json2Ts } from 'json2ts/src/json2ts';
+import { JSONValue } from '~/logic/HTTPArchive/IRequest';
+
 export const getConfig = (key: string, defaultValue: boolean | string | number) => new Promise((resolve) => {
   chrome.storage.local.get([key], (result) => {
     if (result[key] !== undefined) {
@@ -8,3 +11,13 @@ export const getConfig = (key: string, defaultValue: boolean | string | number) 
     }
   });
 });
+
+export const formatJson = (json: JSONValue) => JSON.stringify(json, null, 2);
+
+export const convertJsonToTS = (json: JSONValue) => {
+  try {
+    return new Json2Ts().convert(JSON.stringify(json));
+  } catch (error) {
+    return null;
+  }
+};
