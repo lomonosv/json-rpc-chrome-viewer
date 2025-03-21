889fba7e93a31208923d560399d6b4854045f925
lomonosv <lomonosv.developer@gmail.com>
2025-03-21 14:06:23 +0200

    Websockets support

---
+++ src/content/content.ts
@@ -0,0 +1,20 @@
+const script = document.createElement('script');
+
+script.id = 'websockets-interceptor';
+script.fetchPriority = 'high';
+
+(document.head || document.documentElement).appendChild(script);
+
+script.onload = () => {
+  script.parentNode.removeChild(script);
+};
+
+script.src = chrome.runtime.getURL('content/websockets.js');
+
+window.addEventListener('message', (event) => {
+  if (event.source !== window || !event.data.type) return;
+
+  if (event.data.type === 'JSON_RPC_WEBSOCKET_MESSAGE') {
+    chrome.runtime.sendMessage({ type: 'JSON_RPC_WEBSOCKET_MESSAGE', payload: event.data.payload });
+  }
+});
