573e5d600b11b9b74d9f21c5b935244d75b4c04d
lomonosv <lomonosv.developer@gmail.com>
2025-04-06 14:21:06 +0300

    Mute runtime sendMessage connection errors

--- src/content/content.ts
+++ src/content/content.ts
@@ -2,6 +2,8 @@ window.addEventListener('message', (event) => {
   if (event.source !== window || !event.data.type) return;
 
   if (event.data.type === 'JSON_RPC_WEBSOCKET_MESSAGE') {
-    chrome.runtime.sendMessage({ type: 'JSON_RPC_WEBSOCKET_MESSAGE', payload: event.data.payload });
+    chrome.runtime.sendMessage({ type: 'JSON_RPC_WEBSOCKET_MESSAGE', payload: event.data.payload }, () => {
+      chrome.runtime.lastError;
+    });
   }
 });
