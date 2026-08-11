f7e85751105c432b1507669fff12e2a29158f4c8
Lomonos Vladimir <lomonosv.developer@gmail.com>
2026-08-11 12:46:17 +0300

    feat: resizable and reordable columns

--- src/components/RequestList/Request/Request.tsx
+++ src/components/RequestList/Request/Request.tsx
@@ -2,11 +2,14 @@ import React, { useEffect, useRef } from 'react';
 import cn from 'classnames';
 import { useRequestContext } from '~/logic/HTTPArchive/HttpArchiveContext';
 import { useSettingsContext } from '~/logic/SettingsContext/SettingsContext';
+import { useCacheContext } from '~/logic/CacheContext/CacheContext';
 import useEditRequestModal from './EditRequestModal/useEditRequestModal';
 import Button from '~/components/common/Button';
 import Icon, { IconType } from '~/components/common/Icon';
 import Waterfall from './Waterfall';
 import { getRequestLabel } from '~/logic/HTTPArchive/filters';
+import { SortField } from '~/logic/HTTPArchive/SortField';
+import { ResizableColumn, getColumnWidthStyle } from '../columns';
 import { IRequest } from '~/logic/HTTPArchive/IRequest';
 import styles from './request.scss';
 
@@ -20,6 +23,7 @@ const Request = ({ item, timelineStart, timelineEnd }: IComponentProps) => {
   const rowRef = useRef<HTMLDivElement>(null);
   const { selected, setSelected } = useRequestContext();
   const isSelected = item.uuid === selected?.uuid;
+  const { columnOrder } = useCacheContext();
   const {
     showCorsBadge,
     showWebsocketBadge,
@@ -41,6 +45,41 @@ const Request = ({ item, timelineStart, timelineEnd }: IComponentProps) => {
       rowRef.current?.scrollIntoView({ block: 'nearest' });
     }
   }, [isSelected]);
+
+  const isColumnVisible: Record<ResizableColumn, boolean> = {
+    [SortField.Waterfall]: showWaterfallColumn,
+    [SortField.Status]: showStatusColumn,
+    [SortField.Size]: showSizeColumn,
+    [SortField.Time]: showTimeColumn
+  };
+
+  const visibleColumns = columnOrder.filter((field) => isColumnVisible[field]);
+
+  const renderCellContent = (field: ResizableColumn) => {
+    if (field === SortField.Waterfall) {
+      return (
+        <Waterfall
+          item={ item }
+          timelineStart={ timelineStart }
+          timelineEnd={ timelineEnd }
+        />
+      );
+    }
+
+    if (item.isWebSocket) {
+      return '';
+    }
+
+    if (field === SortField.Status) {
+      return Math.ceil(item.response.status);
+    }
+
+    if (field === SortField.Size) {
+      return Math.ceil(item.response.content.size);
+    }
+
+    return Math.ceil(item.time);
+  };
 
   const handleClick = () => {
     setSelected(item);
@@ -104,30 +143,15 @@ const Request = ({ item, timelineStart, timelineEnd }: IComponentProps) => {
         ) }
       </div>
       <div className={ styles.meta }>
-        { showWaterfallColumn && (
-          <div className={ styles.waterfallCell }>
-            <Waterfall
-              item={ item }
-              timelineStart={ timelineStart }
-              timelineEnd={ timelineEnd }
-            />
+        { visibleColumns.map((field) => (
+          <div
+            key={ field }
+            className={ cn({ [styles.waterfallCell]: field === SortField.Waterfall }) }
+            style={ getColumnWidthStyle(field) }
+          >
+            { renderCellContent(field) }
           </div>
-        ) }
-        { showStatusColumn && (
-          <div>
-            { item.isWebSocket ? '' : Math.ceil(item.response.status) }
-          </div>
-        ) }
-        { showSizeColumn && (
-          <div>
-            { item.isWebSocket ? '' : Math.ceil(item.response.content.size) }
-          </div>
-        ) }
-        { showTimeColumn && (
-          <div>
-            { item.isWebSocket ? '' : Math.ceil(item.time) }
-          </div>
-        ) }
+        )) }
       </div>
       { isEditRequestModalVisible && (
         <EditRequestModal
